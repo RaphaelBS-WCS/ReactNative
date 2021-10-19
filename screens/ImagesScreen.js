1bef47d3d839d454aaea8e45dee8da66664fd038
Niurwa <raphaelbilletservoin@gmail.com>
2021-10-19 18:02:49 +0200

    React Native 04 Done

--- screens/ImagesScreen.js
+++ screens/ImagesScreen.js
@@ -1,8 +1,42 @@
-import React from "react";
-import { Text } from "react-native";
+import * as FileSystem from "expo-file-system";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Image, FlatList } from "react-native";
 
 function ImagesScreen() {
-	return <Text>Images Screen</Text>
+	const [imagesURI, setImagesURI] = useState([]);
+	useEffect(() => {
+		(async () => {
+		  const images = await FileSystem.readDirectoryAsync(
+			FileSystem.cacheDirectory + "ImageManipulator"
+		  );
+		  setImagesURI(images)
+		})();
+	  }, []);
+	return imagesURI.length > 0 ? (
+		<FlatList
+			data={imagesURI}
+			keyExtractor={(imageURI) => imageURI}
+			renderItem={(itemData) => {
+				console.log("item", itemData);
+				return (
+				<Image
+					style={styles.image}
+					source={{
+					uri:
+						FileSystem.cacheDirectory + "ImageManipulator/" + itemData.item,
+					}}
+				/>
+				);
+			}}
+		/>
+	) : null;
 }
+
+const styles = StyleSheet.create({
+	image: {
+	  resizeMode: "cover",
+	  height: 500,
+	},
+  });
 
 export default ImagesScreen;
