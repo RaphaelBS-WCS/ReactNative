139730b4d7885df0ed952a17bf47ff9509435e37
Niurwa <raphaelbilletservoin@gmail.com>
2021-10-20 11:14:56 +0200

    Reactive Native 05 - Upload

--- screens/ImagesScreen.js
+++ screens/ImagesScreen.js
@@ -1,6 +1,7 @@
 import * as FileSystem from "expo-file-system";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Image, FlatList } from "react-native";
+import { StyleSheet, Image, FlatList, TouchableOpacity, Text, View } from "react-native";
+import axios from "axios";
 
 function ImagesScreen() {
 	const [imagesURI, setImagesURI] = useState([]);
@@ -11,7 +12,8 @@ function ImagesScreen() {
 		  );
 		  setImagesURI(images)
 		})();
-	  }, []);
+	}, []);
+
 	return imagesURI.length > 0 ? (
 		<FlatList
 			data={imagesURI}
@@ -19,13 +21,48 @@ function ImagesScreen() {
 			renderItem={(itemData) => {
 				console.log("item", itemData);
 				return (
-				<Image
-					style={styles.image}
-					source={{
-					uri:
-						FileSystem.cacheDirectory + "ImageManipulator/" + itemData.item,
-					}}
-				/>
+					<View style={styles.imageContainer}>
+						<Image
+							style={styles.image}
+							source={{
+							uri:
+								FileSystem.cacheDirectory + "ImageManipulator/" + itemData.item,
+							}}
+						/>
+						<TouchableOpacity 
+							style={styles.button}
+							onPress= {
+								async () => {
+									formData = new FormData();
+									formData.append("fileData", {
+										name: itemData.item,
+										type: "image/jpg",
+										uri: FileSystem.cacheDirectory + "ImageManipulator/" + itemData.item,
+									});
+									let serverResponse;
+									try {
+										serverResponse = await axios({
+											method:'post',
+											url:"https://wildstagram.nausicaa.wilders.dev/upload",
+											data: formData,
+											headers: {
+												"Content-Type": "multipart/form-data; ",
+											},
+										})
+										if (serverResponse.status === 200) {
+											alert("Uploaded");
+										}
+								
+									} catch (err) {
+										console.log(err);
+										alert("Error");
+									}
+								}
+							}
+						>
+						<Text  style={{color: "white"}}>Upload</Text>					
+						</TouchableOpacity>
+					</View>
 				);
 			}}
 		/>
@@ -34,8 +71,16 @@ function ImagesScreen() {
 
 const styles = StyleSheet.create({
 	image: {
-	  resizeMode: "cover",
-	  height: 500,
+		resizeMode: "cover",
+		height: 500,
+	},
+	button: {
+		alignItems: "center",
+		backgroundColor: "grey",
+		padding: 10
+	},
+	imageContainer: {
+		margin: 10
 	},
   });
 
